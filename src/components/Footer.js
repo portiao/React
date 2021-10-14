465ce922344524989b9a994b80e2cbc5bb4b9284
portiao <portiachen81@gmail.com>
2021-10-14 23:59:12 +0800

    add react-router-dom 使用

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 function Footer() {
     return (
-        <div>
-            Portia Chen©
+        <div className="footer">
+            Portia Chen 2021©
         </div>
     );
 };
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import {Link} from "react-router-dom"
 
 function Nav() {
     return (
         <nav>
             <ul>
-                <li><a href="/">Home</a></li>
-                <li><a href="/about">About</a></li>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/about">About</Link></li>
             </ul>
         </nav>
     )
